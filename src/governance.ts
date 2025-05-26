18c184480e761d765f9c079c90b0ffd13bd57b4e
ilotod <openfund365@gmail.com>
2025-05-26 10:38:29 +0900

    init subgraph stele-base2

---
+++ src/governance.ts
@@ -0,0 +1,202 @@
+import {
+  ProposalCreated as ProposalCreatedEvent,
+  ProposalCanceled as ProposalCanceledEvent,
+  ProposalExecuted as ProposalExecutedEvent,
+  ProposalQueued as ProposalQueuedEvent,
+  VoteCast as VoteCastEvent,
+  VoteCastWithParams as VoteCastWithParamsEvent,
+  ProposalThresholdSet as ProposalThresholdSetEvent,
+  QuorumNumeratorUpdated as QuorumNumeratorUpdatedEvent,
+  VotingDelaySet as VotingDelaySetEvent,
+  VotingPeriodSet as VotingPeriodSetEvent,
+  TimelockChange as TimelockChangeEvent
+} from "../generated/SteleGovernor/SteleGovernor"
+
+import { Bytes } from "@graphprotocol/graph-ts"
+
+import {
+  ProposalCreated,
+  ProposalCanceled,
+  ProposalExecuted,
+  ProposalQueued,
+  VoteCast,
+  VoteCastWithParams,
+  ProposalThresholdSet,
+  QuorumNumeratorUpdated,
+  VotingDelaySet,
+  VotingPeriodSet,
+  TimelockChange
+} from "../generated/schema"
+
+export function handleProposalCreated(event: ProposalCreatedEvent): void {
+  let entity = new ProposalCreated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.proposalId = event.params.proposalId
+  entity.proposer = event.params.proposer
+  
+  // Convert Address[] to Bytes[]
+  let targets: Bytes[] = []
+  for (let i = 0; i < event.params.targets.length; i++) {
+    targets.push(event.params.targets[i])
+  }
+  entity.targets = targets
+  
+  entity.values = event.params.values
+  entity.signatures = event.params.signatures
+  entity.calldatas = event.params.calldatas
+  entity.voteStart = event.params.voteStart
+  entity.voteEnd = event.params.voteEnd
+  entity.description = event.params.description
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleProposalCanceled(event: ProposalCanceledEvent): void {
+  let entity = new ProposalCanceled(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.proposalId = event.params.proposalId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleProposalExecuted(event: ProposalExecutedEvent): void {
+  let entity = new ProposalExecuted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.proposalId = event.params.proposalId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleProposalQueued(event: ProposalQueuedEvent): void {
+  let entity = new ProposalQueued(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.proposalId = event.params.proposalId
+  entity.eta = event.params.eta
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVoteCast(event: VoteCastEvent): void {
+  let entity = new VoteCast(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.voter = event.params.voter
+  entity.proposalId = event.params.proposalId
+  entity.support = event.params.support
+  entity.weight = event.params.weight
+  entity.reason = event.params.reason
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVoteCastWithParams(event: VoteCastWithParamsEvent): void {
+  let entity = new VoteCastWithParams(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.voter = event.params.voter
+  entity.proposalId = event.params.proposalId
+  entity.support = event.params.support
+  entity.weight = event.params.weight
+  entity.reason = event.params.reason
+  entity.params = event.params.params
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleProposalThresholdSet(event: ProposalThresholdSetEvent): void {
+  let entity = new ProposalThresholdSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldProposalThreshold = event.params.oldProposalThreshold
+  entity.newProposalThreshold = event.params.newProposalThreshold
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleQuorumNumeratorUpdated(event: QuorumNumeratorUpdatedEvent): void {
+  let entity = new QuorumNumeratorUpdated(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldQuorumNumerator = event.params.oldQuorumNumerator
+  entity.newQuorumNumerator = event.params.newQuorumNumerator
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVotingDelaySet(event: VotingDelaySetEvent): void {
+  let entity = new VotingDelaySet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldVotingDelay = event.params.oldVotingDelay
+  entity.newVotingDelay = event.params.newVotingDelay
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleVotingPeriodSet(event: VotingPeriodSetEvent): void {
+  let entity = new VotingPeriodSet(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldVotingPeriod = event.params.oldVotingPeriod
+  entity.newVotingPeriod = event.params.newVotingPeriod
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleTimelockChange(event: TimelockChangeEvent): void {
+  let entity = new TimelockChange(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.oldTimelock = event.params.oldTimelock
+  entity.newTimelock = event.params.newTimelock
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+} 
